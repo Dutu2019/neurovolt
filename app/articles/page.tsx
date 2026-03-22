2c711223132dc5b51ba6942060b37e04bab6ceec
Eric Tanase <erictanase2007@gmail.com>
2026-03-21 23:29:54 -0400

    debugging

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -5,24 +5,31 @@ import { ArticleContent, getAllArticles } from "@/lib/content";
 export const dynamic = "force-static";
 
 export default async function ArticlesPage() {
-  const articles: ArticleContent[] = await getAllArticles();
-  return (
-    <section>
-      <h1 className="font-serif text-4xl md:text-5xl lg:text-7xl font-bold text-primary text-center pt-10 z-11">
-        NOS ARTICLES
-      </h1>
-      <div className="relative flex flex-col gap-6 max-w-7xl m-auto py-20 align-center">
-        {articles.map((article, i) => (
-          <ArticleItem
-            key={article.slug}
-            title={article.title}
-            description={article.description || ""}
-            slug={article.slug}
-            imageSrc={article.coverImage && article.coverImage?.url}
-            inverse={i % 2 === 0}
-          />
-        ))}
-      </div>
-    </section>
-  );
+  try {
+    const articles: ArticleContent[] = await getAllArticles();
+    return (
+      <section>
+        <h1 className="font-serif text-4xl md:text-5xl lg:text-7xl font-bold text-primary text-center pt-10 z-11">
+          NOS ARTICLES
+        </h1>
+        <div className="relative flex flex-col gap-6 max-w-7xl m-auto py-20 align-center">
+          {articles.map((article, i) => (
+            <ArticleItem
+              key={article.slug}
+              title={article.title}
+              description={article.description || ""}
+              slug={article.slug}
+              imageSrc={article.coverImage && article.coverImage?.url}
+              inverse={i % 2 === 0}
+            />
+          ))}
+        </div>
+      </section>
+    );
+  } catch (error) {
+    console.error("Error fetching articles:", error);
+    return (
+      <div className="text-center text-red-500">Failed to load articles.</div>
+    );
+  }
 }
