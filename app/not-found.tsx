1a7f69e5621d95253d56b4deea17d52ce8853cd7
Eric Tanase <erictanase2007@gmail.com>
2026-03-22 00:10:08 -0400

    fixed ts issue fr this time

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,52 +1,13 @@
 "use client";
 
-import { Area, AreaChart } from "recharts";
-
-import { ChartConfig, ChartContainer } from "@/components/ui/chart";
-
-const chartData = [
-  { month: "January", desktop: 186 },
-  { month: "February", desktop: 305 },
-  { month: "March", desktop: 237 },
-  { month: "April", desktop: 73 },
-  { month: "May", desktop: 209 },
-  { month: "June", desktop: 214 },
-];
-
-const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "var(--chart-6)",
-  },
-} satisfies ChartConfig;
-
 export default function NotFound() {
   return (
-    <section className="relative h-screen w-full bg-[var(--primary)] contain-content">
-      <h1 className="relative w-fit m-auto pt-150 text-6xl md:text-9xl font-extrabold italic z-10">
-        En développement...
-      </h1>
-      <ChartContainer
-        config={chartConfig}
-        className="absolute min-h-[200px] w-full -translate-y-[25%] scale-110"
-      >
-        <AreaChart
-          accessibilityLayer
-          data={chartData}
-          margin={{
-            left: 12,
-            right: 12,
-          }}
-        >
-          <Area
-            dataKey="desktop"
-            type="natural"
-            fill="var(--color-primary)"
-            fillOpacity={1}
-            stroke="var(--color-primary)"
-          />
-        </AreaChart>
-      </ChartContainer>
+    <section className="relative h-screen w-full bg-(--primary) contain-content">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+        <h1 className="text-6xl font-bold text-white mb-4">
+          404 - Page Not Found
+        </h1>
+      </div>
     </section>
   );
 }
